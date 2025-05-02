c03d5a92995b5834bc3de8ebe6445928891658c4
coolrunner1 <waurttttt@gmail.com>
2025-05-02 20:31:33 +0300

    Finished leaky bucket

--- cmd/applicationCLI.go
+++ cmd/applicationCLI.go
@@ -3,7 +3,10 @@ package cmd
 import (
 	"fmt"
 	"github.com/coolrunner1/project/utils/filter"
+	"github.com/coolrunner1/project/utils/limiter"
+	"github.com/go-errors/errors"
 	"github.com/spf13/cobra"
+	"time"
 )
 
 func ApplicationCliInit() {
@@ -28,6 +31,46 @@ func ApplicationCliInit() {
 	}
 
 	rootCmd.AddCommand(bloomTestCmd)
+
+	leakyBucketTestCmd := &cobra.Command{
+		Use: "leakyBucketTest",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Leaky Bucket Test")
+			leakyBucket := limiter.NewLeakyBucket(2000, 10)
+			err := leakyBucket.AddPacket(100)
+			if err != nil {
+				fmt.Println(err.(*errors.Error).ErrorStack())
+				return
+			}
+			err = leakyBucket.AddPacket(100)
+			if err != nil {
+				fmt.Println(err.(*errors.Error).ErrorStack())
+				return
+			}
+			err = leakyBucket.AddPacket(1000)
+			if err != nil {
+				fmt.Println(err.(*errors.Error).ErrorStack())
+				return
+			}
+
+			go leakyBucket.Run()
+			time.Sleep(1 * time.Second)
+			err = leakyBucket.AddPacket(200)
+			if err != nil {
+				fmt.Println(err.(*errors.Error).ErrorStack())
+				return
+			}
+			err = leakyBucket.AddPacket(1000)
+			if err != nil {
+				fmt.Println(err.(*errors.Error).ErrorStack())
+				return
+			}
+			time.Sleep(1 * time.Second)
+			leakyBucket.Stop()
+		},
+	}
+
+	rootCmd.AddCommand(leakyBucketTestCmd)
 
 	bloomCmd := &cobra.Command{
 		Use: "bloom",
@@ -93,6 +136,67 @@ func ApplicationCliInit() {
 	}
 
 	rootCmd.AddCommand(bloomCmd)
+
+	leakyBucketCmd := &cobra.Command{
+		Use: "leakyBucket",
+		Run: func(cmd *cobra.Command, args []string) {
+			var bucketSize uint32
+			fmt.Println("Enter the bucket size")
+			_, err := fmt.Scan(&bucketSize)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			var tickRate time.Duration
+			fmt.Println("Enter the tick rate")
+			_, err = fmt.Scan(&tickRate)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			leakyBucket := limiter.NewLeakyBucket(bucketSize, tickRate)
+			for {
+				fmt.Println("1 - Send a new packet")
+				fmt.Println("2 - Start the leaky bucket")
+				fmt.Println("3 - Stop the leaky bucket")
+				fmt.Println("0 - Quit the application")
+
+				var choice uint32
+				_, err := fmt.Scan(&choice)
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+
+				switch choice {
+				case 1:
+					fmt.Println("Enter the size of the packet")
+					var packetSize uint32
+					_, err := fmt.Scan(&packetSize)
+					if err != nil {
+						fmt.Println(err)
+						return
+					}
+					err = leakyBucket.AddPacket(packetSize)
+					if err != nil {
+						fmt.Println(err.(*errors.Error).ErrorStack())
+						return
+					}
+				case 2:
+					go leakyBucket.Run()
+				case 3:
+					leakyBucket.Stop()
+				case 0:
+					return
+				default:
+					fmt.Println("Invalid input")
+				}
+			}
+		},
+	}
+
+	rootCmd.AddCommand(leakyBucketCmd)
 
 	err := rootCmd.Execute()
 	if err != nil {
--- utils/limiter/leakyBucket.go
+++ utils/limiter/leakyBucket.go
@@ -2,6 +2,7 @@ package limiter
 
 import (
 	"fmt"
+	"github.com/go-errors/errors"
 	"sync"
 	"time"
 )
@@ -9,39 +10,61 @@ import (
 type LeakyBucket struct {
 	bucketSize uint32
 	//Queue of packets
-	bucket   []uint32
-	mutex    sync.Mutex
-	tickRate time.Duration
+	bucket []uint32
+	mutex  sync.Mutex
+	ticker *time.Ticker
+	done   chan bool
 }
 
-func NewLeakyBucket(bucketSize uint32) *LeakyBucket {
+func NewLeakyBucket(bucketSize uint32, tickRate time.Duration) *LeakyBucket {
 	return &LeakyBucket{
 		bucketSize: bucketSize,
-		bucket:     make([]uint32, bucketSize),
+		bucket:     make([]uint32, 0),
+		ticker:     time.NewTicker(tickRate * time.Millisecond),
+		done:       make(chan bool),
 	}
 }
 
-func (lb *LeakyBucket) AddPacket(packetSize uint32) {
-	if lb.getCurrentBucketSize() >= lb.bucketSize {
-		fmt.Println("Bucket is full")
-		return
+func (lb *LeakyBucket) AddPacket(packetSize uint32) error {
+	lb.mutex.Lock()
+	defer lb.mutex.Unlock()
+
+	currentBucketSize := lb.getCurrentBucketSize()
+	if currentBucketSize == lb.bucketSize {
+		return errors.New("Bucket is full")
+	} else if currentBucketSize+packetSize > lb.bucketSize {
+		return errors.New("Packet is too large")
 	}
 	lb.pushPacket(packetSize)
+	return nil
 }
 
 func (lb *LeakyBucket) Run() {
 	for {
-		if len(lb.bucket) == 0 {
-			fmt.Println("Bucket is empty\n Exiting...")
+		select {
+		case <-lb.done:
+			fmt.Println("Leaky Bucket stopped")
+			lb.ticker.Stop()
 			return
+		case <-lb.ticker.C:
+			lb.mutex.Lock()
+			if len(lb.bucket) > 0 {
+				packet := lb.popPacket()
+				fmt.Printf("Packet with a size of %d has been popped\n", packet)
+			}
+			lb.mutex.Unlock()
 		}
 	}
 }
 
+func (lb *LeakyBucket) Stop() {
+	lb.done <- true
+}
+
 func (lb *LeakyBucket) getCurrentBucketSize() uint32 {
 	var bucketSize uint32
-	for i := range lb.bucket {
-		bucketSize += lb.bucket[i]
+	for _, el := range lb.bucket {
+		bucketSize += el
 	}
 	return bucketSize
 }
@@ -51,6 +74,10 @@ func (lb *LeakyBucket) pushPacket(packetSize uint32) {
 }
 
 func (lb *LeakyBucket) popPacket() uint32 {
+	if len(lb.bucket) == 0 {
+		fmt.Println("Bucket is empty")
+		return 0
+	}
 	el := lb.bucket[0]
 	lb.bucket = lb.bucket[1:]
 	return el
